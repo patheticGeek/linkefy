567b6fb653407c18f4c22ef5f1ba09f5590e127a
patheticGeek <shivamnagi14@gmail.com>
2019-04-20 21:03:57 +0530

    ..

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
