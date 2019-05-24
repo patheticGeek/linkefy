caa31e16233c9d61f4d00ae9d487d98a1474d4de
patheticGeek <shivamnagi14@gmail.com>
2019-05-24 19:37:56 +0530

    .envfied

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/storage';
-
+// Replace with your config
 const config = {
-  apiKey: 'AIzaSyA14hPpGo27Q6DWMvo-Y42eMrBbswuyQPA',
-  authDomain: 'linkefy-14.firebaseapp.com',
-  databaseURL: 'https://linkefy-14.firebaseio.com',
-  projectId: 'linkefy-14',
-  storageBucket: 'linkefy-14.appspot.com',
-  messagingSenderId: '312256927027'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
