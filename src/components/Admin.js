8b97be55abd66028c5ea6cf261d039cecc9bfec6
Pathetic Geek <geekpathetic@gmail.com>
2020-07-03 17:12:33 +0530

    fixes

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,51 +1,48 @@
-import React, { Component } from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import AddLinks from './AddLinks';
-import Settings from './Settings';
-import '../assets/animate.css';
-import '../assets/admin.css';
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import AddLinks from "./AddLinks";
+import Settings from "./Settings";
+import "../assets/animate.css";
+import "../assets/admin.css";
 
 class Admin extends Component {
-  state = { toastShown: false, toastType: '', toastMessage: '' };
+  state = { toastShown: false, toastType: "", toastMessage: "" };
 
   componentWillReceiveProps(newProps) {
     if (newProps.success) {
-      this.setState({ toastShown: true, toastType: 'success', toastMessage: newProps.message });
+      this.setState({ toastShown: true, toastType: "success", toastMessage: newProps.message });
       setTimeout(() => this.setState({ toastShown: false }), 3000);
     }
     if (newProps.error) {
-      this.setState({ toastShown: true, toastType: 'error', toastMessage: newProps.message });
+      this.setState({ toastShown: true, toastType: "error", toastMessage: newProps.message });
       setTimeout(() => this.setState({ toastShown: false }), 3000);
     }
   }
 
   render() {
-    if (!this.props.logedin) return <Redirect to='/' />;
-    if (this.props.user === null) return <Redirect to='/' />;
-    if (this.props.user.username === '') return <Redirect to='/signin' />;
+    if (!this.props.logedin) return <Redirect to="/" />;
+    if (this.props.user === null) return <Redirect to="/" />;
+    if (this.props.user.username === "") return <Redirect to="/signin" />;
     return (
-      <main className='admin'>
-        <div className='admin-nav'>
-          <Link to='/' className='logo'>
-            <img src='/icon_512.png' alt='logo' />
+      <main className="admin">
+        <div className="admin-nav">
+          <Link to="/" className="logo">
+            <img src="/icon_512.png" alt="logo" />
           </Link>
-          <div className='admin-links'>
-            <div className='admin-nav-bio-link mobile'>
+          <div className="admin-links">
+            <div className="admin-nav-bio-link mobile">
               <h3>My Bio Link: </h3>
               <div>
-                <a href={'https://linkefy.tk/' + this.props.user.username}>
-                  {'https://linkefy.tk/' + this.props.user.username}
+                <a href={window.location.origin + "/" + this.props.user.username}>
+                  {window.location.origin + "/" + this.props.user.username}
                 </a>
               </div>
             </div>
-            <div className='nav'>
-              <Link
-                className={this.props.page === 'links' ? 'admin-nav-link active' : 'admin-nav-link'}
-                to='/admin'
-              >
-                <span className='admin-nav-link-text'>My Links</span>
-                <span className='admin-nav-link-line' />
+            <div className="nav">
+              <Link className={this.props.page === "links" ? "admin-nav-link active" : "admin-nav-link"} to="/admin">
+                <span className="admin-nav-link-text">My Links</span>
+                <span className="admin-nav-link-line" />
               </Link>
               {/* <Link
                 className={
@@ -56,32 +53,29 @@ class Admin extends Component {
                 <span className="admin-nav-link-text">Analytics</span>
                 <span className="admin-nav-link-line" />
               </Link> */}
-              <Link
-                className={this.props.page === 'settings' ? 'admin-nav-link active' : 'admin-nav-link'}
-                to='/admin/settings'
-              >
-                <span className='admin-nav-link-text'>Settings</span>
-                <span className='admin-nav-link-line' />
+              <Link className={this.props.page === "settings" ? "admin-nav-link active" : "admin-nav-link"} to="/admin/settings">
+                <span className="admin-nav-link-text">Settings</span>
+                <span className="admin-nav-link-line" />
               </Link>
             </div>
           </div>
-          <div className='admin-nav-divider' />
-          <div className='admin-nav-bio-link'>
+          <div className="admin-nav-divider" />
+          <div className="admin-nav-bio-link">
             <div>
               <h3>My Bio Link: </h3>
-              <a href={'https://linkefy.tk/' + this.props.user.username}>
-                {'https://linkefy.tk/' + this.props.user.username}
+              <a href={window.location.origin + "/" + this.props.user.username}>
+                {window.location.origin + "/" + this.props.user.username}
               </a>
             </div>
           </div>
         </div>
 
-        {this.props.page === 'links' ? <AddLinks /> : null}
-        {this.props.page === 'settings' ? <Settings /> : null}
+        {this.props.page === "links" ? <AddLinks /> : null}
+        {this.props.page === "settings" ? <Settings /> : null}
 
-        <div className={'toast ' + this.state.toastType + ' ' + (this.state.toastShown ? 'shown' : '')}>
-          <div className={'icon ' + this.state.toastType} />
-          <div className='text'>{this.state.toastMessage}</div>
+        <div className={"toast " + this.state.toastType + " " + (this.state.toastShown ? "shown" : "")}>
+          <div className={"icon " + this.state.toastType} />
+          <div className="text">{this.state.toastMessage}</div>
         </div>
       </main>
     );
@@ -93,5 +87,5 @@ export default connect(({ logedin, user, success, error, message }) => ({
   user,
   success,
   error,
-  message
+  message,
 }))(Admin);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,61 +1,49 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import Navbar from './Navbar';
-import '../assets/home.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import "../assets/home.css";
 
 class Home extends Component {
   componentDidMount() {
-    document.body.classList.toggle('home');
+    document.body.classList.toggle("home");
   }
   componentWillUnmount() {
-    document.body.classList.toggle('home');
+    document.body.classList.toggle("home");
   }
 
   render() {
     return (
       <React.Fragment>
         <Navbar />
-        <main className='home'>
-          <header className='page-header'>
-            <div className='squares square1' />
-            <div className='squares square2' />
-            <div className='squares square3' />
-            <div className='squares square4' />
-            <div className='squares square5' />
-            <div className='squares square6' />
-            <div className='squares square7' />
-            <div className='container'>
-              <img src='./icon_512.png' alt='logo' />
+        <main className="home">
+          <header className="page-header">
+            <div className="squares square1" />
+            <div className="squares square2" />
+            <div className="squares square3" />
+            <div className="squares square4" />
+            <div className="squares square5" />
+            <div className="squares square6" />
+            <div className="squares square7" />
+            <div className="container">
+              <img src="./icon_512.png" alt="logo" />
               <h1>Linkefy</h1>
               <h3>One place for your linking needs</h3>
               {this.props.logedin ? (
-                <Link to='/admin' className='button primary'>
+                <Link to="/admin" className="button primary">
                   Go to my profile
                 </Link>
               ) : (
                 <>
-                  <Link to='/signin' className='button primary'>
+                  <Link to="/signin" className="button primary">
                     Sign up
                   </Link>
-                  <Link to='/signin' className='button'>
+                  <Link to="/signin" className="button">
                     Login
                   </Link>
                 </>
               )}
             </div>
-          </header>
-          <header id='features'>
-            <section className='feature'>
-              <div className='text'>
-                <h2>Full control of the links</h2>
-                <p>
-                  Add an icon to show a peek of the product/page you are linking and an animation to the link
-                  to grab attention of the user.
-                </p>
-              </div>
-              <div className='image' />
-            </section>
           </header>
         </main>
       </React.Fragment>
@@ -64,5 +52,5 @@ class Home extends Component {
 }
 
 export default connect(({ logedin }) => ({
-  logedin
+  logedin,
 }))(Home);
